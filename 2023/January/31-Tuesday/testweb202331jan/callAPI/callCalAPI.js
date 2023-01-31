9d1e06b871c2f0888b5a7bcaf0ee69d9dcadb75a
chamiporn@reinvention.online <79304194+reseac-suilui-c@users.noreply.github.com>
2023-01-31 17:55:58 +0700

    #6

---
+++ 2023/January/31-Tuesday/testweb202331jan/callAPI/callCalAPI.js
@@ -0,0 +1,16 @@
+// function calculator
+
+const httpS = require('http');
+const portS = process.env.portS || 1000;
+
+const startedSV = httpS.createServer();
+
+startedSV.listen(portS);
+
+console.log(startedSV);
+console.log('port: ' + portS);
+
+// get
+
+// post
+
