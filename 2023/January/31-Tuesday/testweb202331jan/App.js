892a84c076ca2fb747bb34ab3aa00317446febc7
chamiporn@reinvention.online <79304194+reseac-suilui-c@users.noreply.github.com>
2023-02-01 11:11:10 +0700

    #1 Modify Web demo

--- 2023/January/31-Tuesday/testweb202331jan/App.js
+++ 2023/January/31-Tuesday/testweb202331jan/App.js
@@ -1,10 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
+
+// import file API
+import { callFunctionGet } from './callAPI/callCalAPI'
+
 
 export default function App() {
+
+  const [values, setVar] = React.useState();
+
+  var dataIN = {
+    A: 10,
+    B: 2.5
+  }
+
+  // syntax
+  // const name_Func = () => {
+  // name_FuncImport().then((var_collec_data) => {
+  //     setVar_in_useState(var_collec_data)
+  //   })
+  // }
+
+  const useCalGet = () => {
+    callFunctionGet().then((resultGet) => {
+      setVar(resultGet)
+    });
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Welcome..... to my world 🐼</Text>
+
+      <Text>{JSON.stringify(values)}</Text>
+
+      <Button onPress={() => useCalGet()}>*🧧*</Button>
+
       <StatusBar style="auto" />
     </View>
   );
--- 2023/January/31-Tuesday/testweb202331jan/callAPI/callCalAPI.js
+++ 2023/January/31-Tuesday/testweb202331jan/callAPI/callCalAPI.js
@@ -1,6 +1,40 @@
 // function calculator
+const callAxios = require('axios');
+// set host value
+var setHost = 'http://localhost:2000'
 
 // get
+// syntax
+// data type [name func] = () =>
+// fetch(path, {json data
+// }).then((variable) => variable.json() [**create var for collect data in json format**] ).then([**convert variable to use for Export**]  (var return) => {
+//      retuen var return; } ).catch( (var error) => { //* log or anything *//});
+export const callFunctionGet = () =>
+        fetch(setHost + '/', {
+                method: "GET",
+                headers: {
+                        Accept: "application/json",
+                        "Content-Type": "application/json",
+                },
+        }).then((resCalGet) => resCalGet.json()).then((returnRes) => {
+                return returnRes;
+        }).catch((err_res) => {
+                console.log(err_res);
+        });
 
+// ****************************************************************************
 // post
+export const callFunctionPost = () =>
+        fetch(setHost + '/', {
+                method: "POST",
+                headers: {
+                        Accept: "application/json",
+                        "Content-Type": "application/json",
+                },
+        }).then((resCalGet) => resCalGet.json()).then((returnRes) => {
+                return returnRes;
+        }).catch((err_res) => {
+                console.log(err_res);
+        });
 
+// ****************************************************************************
