c047cc1481706d113bdee70c118a0ffb374e97b4
chamiporn@reinvention.online <79304194+reseac-suilui-c@users.noreply.github.com>
2023-12-08 14:09:14 +0700

    #---

--- 2023/November/learn-express/index.js
+++ 2023/November/learn-express/index.js
@@ -1,7 +1,9 @@
 const     express = require("express")
 const     mongoose = require("mongoose")
+const     routes = require("./routes")
 
 const     app = express()
+app.use("/api", routes)
 app.listen( 5000, () => {console.log("Server has started!!")} )
 
 // Connnect to Mongoose
--- 2023/November/learn-express/routes.js
+++ 2023/November/learn-express/routes.js
@@ -1,4 +1,25 @@
 const express = require("express")
+const Post = require("./models/Post") // new
 const router = express.Router()
+
+// Get all posts
+// router.get("/posts", async (req, res) => {
+// 	const posts = await Post.find()
+// 	res.send(posts)
+// })
+
+router.post("/posts", async (req, res) => {
+	const post = new Post({
+		title: req.body.title,
+		content: req.body.content,
+	})
+	await post.save()
+	res.send(post)
+})
+
+router.get("/posts/:id", async (req, res) => {
+	const post = await Post.findOne({ _id: req.params.id })
+	res.send(post)
+})
 
 module.exports = router
