fd3dd3fa1bf17b66983feb0846f5de11e5b55285
chamiporn@reinvention.online <79304194+reseac-suilui-c@users.noreply.github.com>
2023-12-08 13:48:38 +0700

    ---

--- 2023/November/learn-express/index.js
+++ 2023/November/learn-express/index.js
@@ -1,13 +1,13 @@
 const     express = require("express")
 const     mongoose = require("mongoose")
 
-// const     app = express()
-// app.listen( 5000, () => {console.log("Server has started!!")} )
+const     app = express()
+app.listen( 5000, () => {console.log("Server has started!!")} )
 
 // Connnect to Mongoose
-mongoose
-.connect("mongodb://localhost:27017/acmedb", { useNewUrlParser: true })
-.then( () => {
-     const     app = express()
-     app.listen( 5000, () => {console.log("Server has started!!")} )
-})
+// mongoose
+// .connect("http://localhost:3000", { useNewUrlParser: true })
+// .then( () => {
+//      const     app = express()
+//      app.listen( 5000, () => {console.log("Server has started!!")} )
+// })
