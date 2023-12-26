18cc46308cc3fd54ec9d44ab22f4a607a7864aa0
chamiporn@reinvention.online <79304194+reseac-suilui-c@users.noreply.github.com>
2023-12-26 11:36:01 +0700

    #--- 2023/26/December

--- 2023/December/test-api-via-topcoder/server.js
+++ 2023/December/test-api-via-topcoder/server.js
@@ -1,13 +1,18 @@
-const     express = require(express)
-
+//ทำการ import express เข้ามาใช้งาน โดยสร้างตัวแปร express ขึ้นมาเพื่อรับค่า
+const     express = require('express')
+//ทำการสร้าง Instance ของ express และสร้างตัวแปร app ขึ้นมาเพื่อรับค่า
 const     app = express()
-
-const     Port = process.env.Port || 8008
-
-app.get('/', (req, res) => res.send('**********'))
-
+//สร้างตัวแปร PORT ขึ้นมารับค่า port ในกรณีที่เราได้กำหนดไว้ใน environment ของเครื่อง
+//แต่ถ้าไม่ได้กำหนดไว้ เราจะใช้ค่า 8008 แทน
+const     Port = process.env.PORT || 8008
+//สร้าง route ขึ้นมา 1 ตัว โดยกำหนดให้ path คือ / หรือ index ของ host นั่นเอง
+//จากนั้นให้กำหนด response แสดงคำว่า Hello World
+app.get('/', (req, res) => res.send('***** hi there *****'))
+//run web server ที่เราสร้างไว้ โดยใช้ PORT ที่เรากำหนดไว้ในตัวแปร PORT
 app.listen(Port, () => {
-     console.log('Server is running on port : ${Port}')
+      //หากทำการ run server สำเร็จ ให้แสดงข้อความนี้ใน cmd หรือ terminal
+     console.log('Server is running on port : ' + Port)
 })
-
+//ทำการ export app ที่เราสร้างขึ้น เพื่อให้สามารถนำไปใช้งานใน project อื่นๆ 
+//เปรียบเสมือนเป็น module ตัวนึง
 module.exports = app
